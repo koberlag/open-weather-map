ffcb1c5c6830f0259bd7d30da7d9d63a03d08511
Kevin Oberlag <koberlag@gmail.com>
2015-12-13 16:08:20 -0600

    oved bower files, and added routes

---
+++ app/owm-app.js
@@ -0,0 +1,34 @@
+angular.module('OWMApp', ['ngRoute'])
+    .run(function($rootScope, $location) {
+    	$rootScope.$on('$routeChangeError', function() {
+        	$location.path('/error');
+    	});
+	})
+    .config(['$routeProvider', function($routeProvider){
+        $routeProvider.when('/', {
+            templateUrl : 'home.html',
+            controller : 'HomeCtrl'
+        }).when('/cities/:city', {
+        	templateUrl : 'city.html',
+        	controller : 'CityCtrl',
+        	resolve : {
+        		city : function(owmCities, $route, $location){
+        			var city = $route.current.params.city;
+        			if(owmCities.indexOf(city) == -1){
+        				$location.path("/error");
+        				return;
+        			}
+        			return city;
+        		}
+        	}
+        }).when('/error', {
+    		template : '<p>Error - Page Not Found</p>'
+		}).otherwise('/error');
+    }])
+    .value('owmCities', ['New York', 'Dallas','Chicago'])
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+    .controller('CityCtrl', function($scope, city){
+		$scope.city = city;
+    });
